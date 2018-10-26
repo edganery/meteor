ad34d1e6d80057f28cc879c7480ccef3e21ae57d
Edgard <edganery@hotmail.com>
2018-10-26 16:36:17 -0300

    import reacdom and upgrado to App2 with container

--- images/client/main.js
+++ images/client/main.js
@@ -1,10 +1,19 @@
 // Any Js in here is automatically ran for us
 
 // Import the React library
-import React from 'react'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 // Create a component
-
+const App = () => {
+ return {
+   <div>
+   React App #2
+   </div>
+  };
+};
 
 
 // render tis component to the screen
+Meteor.startup {
+ReactDOM.render (<App />, document.querySelector ('.container'));
+}
