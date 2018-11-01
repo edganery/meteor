7844e79bf797bdcd77b031d986f5250d0d64cac8
Edgard <edganery@hotmail.com>
2018-10-31 22:18:39 -0300

    ImageList the first component

---
+++ images/client/components/image_list.js
@@ -0,0 +1,15 @@
+//Create our image list component
+//Import React
+import React from 'react';
+
+//Create our component
+const Image List = () => {
+  return (
+    <url>
+     <li>Image Detail </li>
+     </ul>
+  );
+};
+
+//Export our component
+export default ImageList;
