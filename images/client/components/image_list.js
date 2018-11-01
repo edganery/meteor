c6b6f049ef25a45ebff7082729649d1cae18aa66
Edgard <edganery@hotmail.com>
2018-10-31 22:38:34 -0300

    create image detail file after code import imageDetail

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -1,12 +1,12 @@
 //Create our image list component
 //Import React
 import React from 'react';
-
+importe ImgageDetail from './image_detail'; //dont use .js here
 //Create our component
 const ImageList = () => {
   return (
     <ul>
-     <li>Image Detail </li>
+     <ImageDetail />
      </ul>
   );
 };
