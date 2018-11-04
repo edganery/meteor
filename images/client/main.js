460e7ddedc8d2927db9ecbbc4367626a2406f1eb
Edgard <edganery@hotmail.com>
2018-11-03 23:11:07 -0300

    image detail run

--- images/client/main.js
+++ images/client/main.js
@@ -4,11 +4,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ImageList from './components/image_list';
+
+
 // Create a component
 const App = () => {
  return (
    <div>
-    <h1>Image List </h1>
+    <ImageList />
    </div>
  );
 };
