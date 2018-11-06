764f2c134befdc81d4f6746ccdc51556d8971bd3
Edgard <edganery@hotmail.com>
2018-11-06 17:12:03 -0200

    s2A16 crash at 3:27 after create image detail function

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -3,12 +3,21 @@
 import React from 'react';
 import ImageDetail from './image_detail';
 
+const IMAGES = [
+  { title: 'Pen', link: 'https://dummyimage.com/600x400'},
+  { title: 'Pine Tree', link: 'https://dummyimage.com/600x400'},
+  { title: 'Mug', link: 'https://dummyimage.com/600x400'}
+];
 
 //Create our component
 const ImageList = () => {
-  return (
+  const RenderedImages = IMAGES.map(function(image) {
+    return <ImageDetail image={image} />
+});
+
+    return (
     <ul>
-     <ImageDetail />
+     {RenderedImages}
     </ul>
   );
 };
