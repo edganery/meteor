649d26ee82b072357f93c63be7199ab3ec15099a
Edgard <edganery@hotmail.com>
2018-11-13 11:45:30 -0200

    sintax fix, flickr api, new crash

--- images/client/main.js
+++ images/client/main.js
@@ -3,8 +3,9 @@
 // Import the React library
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from axios;
 import ImageList from './components/image_list';
-import axios from 'axios';
+
 
 // Create a component
 const App = () => {
@@ -19,6 +20,6 @@ const App = () => {
 // render tis component to the screen
 Meteor.startup(() => {
 ReactDOM.render (<App />, document.querySelector ('.container'));
-axios.get(https://www.flickr.com/services/api/tos/)
+axios.get('https://www.flickr.com/services/api/tos/')
 	.then(response => console.log(response));
 });
