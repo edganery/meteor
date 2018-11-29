66ee49f9c0e81b4ab90b3e5231d6a9968570e5cf
Edgard <edganery@hotmail.com>
2018-11-29 20:08:43 -0200

    s25 change axios to fantastic place to loaddata

--- images/client/main.js
+++ images/client/main.js
@@ -11,6 +11,11 @@ import ImageList from './components/image_list';
 class App extends Component {
  componentWillMount(){
    // THIS IS MAGIC! NOW I HAVE OVERPOWERS!! TO LOAD ALL DATA TOGHETER BULHATCHAKA!
+   axios.get('https://api.imgur.com/3/gallery/hot/viral/0', {
+     headers: {
+       Authorization: 'Bearer e85fa4b2470d724ce19af392bf3bd34fdef8a781'
+     }
+   }).then(response => console.log(response));
    console.log ('bug sobre o render fazendo o crash');
  }
 
@@ -26,9 +31,5 @@ class App extends Component {
 // render tis component to the screen
 Meteor.startup(() => {
   ReactDOM.render (<App />, document.querySelector ('.container'));
-  axios.get('https://api.imgur.com/3/gallery/hot/viral/0', {
-    headers: {
-      Authorization: 'Bearer e85fa4b2470d724ce19af392bf3bd34fdef8a781'
-    }
-  }).then(response => console.log(response));
+
 });
