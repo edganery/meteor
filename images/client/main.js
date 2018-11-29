e91fce8641695adce43937c8ea8ac0b99a660a4c
Edgard <edganery@hotmail.com>
2018-11-29 20:21:10 -0200

    s25 setstate sucessfull

--- images/client/main.js
+++ images/client/main.js
@@ -16,14 +16,14 @@ class App extends Component {
     // here brah! rem -
   }
 
- componentWillMount(){
+ componentWillMount() {
    // THIS IS MAGIC! NOW I HAVE OVERPOWERS!! TO LOAD ALL DATA TOGHETER BULHATCHAKA!
    axios.get('https://api.imgur.com/3/gallery/hot/viral/0', {
      headers: {
        Authorization: 'Bearer e85fa4b2470d724ce19af392bf3bd34fdef8a781'
      }
    }).then(response => this.setState({ images: response.data.data }));
-// rem do this-
+// rem never do this-
 // this.state.images = [ {}, {} ];
    console.log ('AQUI EM BAIXO APARECE A MAGIA');
  }
