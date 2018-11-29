a843d619b0a0f2bacd4eb39fd15b4e051aade7c4
Edgard <edganery@hotmail.com>
2018-11-29 20:19:09 -0200

    s24 inclue states and APIobjects goodruun!

--- images/client/main.js
+++ images/client/main.js
@@ -9,17 +9,27 @@ import ImageList from './components/image_list';
 
 // Create a component
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { images: []};
+    // here brah! rem -
+  }
+
  componentWillMount(){
    // THIS IS MAGIC! NOW I HAVE OVERPOWERS!! TO LOAD ALL DATA TOGHETER BULHATCHAKA!
    axios.get('https://api.imgur.com/3/gallery/hot/viral/0', {
      headers: {
        Authorization: 'Bearer e85fa4b2470d724ce19af392bf3bd34fdef8a781'
      }
-   }).then(response => console.log(response));
-   console.log ('bug sobre o render fazendo o crash');
+   }).then(response => this.setState({ images: response.data.data }));
+// rem do this-
+// this.state.images = [ {}, {} ];
+   console.log ('AQUI EM BAIXO APARECE A MAGIA');
  }
 
  render() {
+   console.log(this.state.images);
   return (
    <div>
     <ImageList />
