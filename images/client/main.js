7c147db527e0a6b9d569a3ab71a49d7e52b6f53c
Edgard <edganery@hotmail.com>
2018-11-29 19:54:25 -0200

    s22 class component bug

--- images/client/main.js
+++ images/client/main.js
@@ -1,28 +1,29 @@
 // Any Js in here is automatically ran for us
 
 // Import the React library
-import React from 'react';
+import React, {component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
 
 
 // Create a component
-const App = () => {
- return (
+class App extends component {
+ render() {
+  return (
    <div>
     <ImageList />
    </div>
- );
+  );
+ }
 };
-
 
 // render tis component to the screen
 Meteor.startup(() => {
   ReactDOM.render (<App />, document.querySelector ('.container'));
   axios.get('https://api.imgur.com/3/gallery/hot/viral/0', {
     headers: {
-      Authorization: 'Bearer e85fa4b2470d724ce19af392bf3bd34fdef8a781' 
-    }  
+      Authorization: 'Bearer e85fa4b2470d724ce19af392bf3bd34fdef8a781'
+    }
   }).then(response => console.log(response));
 });
