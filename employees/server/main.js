1fc1435bed4ad8cccb81d62278bedcbd57dfc7cb
Edgard <edganery@hotmail.com>
2018-12-14 13:41:50 -0200

    import collections and creating mdb colle after generating data with faker

--- employees/server/main.js
+++ employees/server/main.js
@@ -1,5 +1,25 @@
+
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '..imports/collections/employees';
+import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  // Great place to generate data
+
+  // Chech to see if data exists in the collection
+   // See if the collection has any records
+  const numberRecords = Employees.find({}).cont();
+  if (!numberRecords) {
+    // Generate some daa...
+    _.times(5000, () => {
+      const { name, email, phone } = helpers.createCard();
+
+      Employees.insert({
+        name, email, phone,
+        avatar: image.avatar()
+      });
+    });
+  };
 });
