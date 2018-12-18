45983a193c92b6d0e9a69afef9bd2ba7fe7cabda
Edgard <edganery@hotmail.com>
2018-12-18 00:37:15 -0200

    s3 a37 generating data with faker but close full code, add import meteor

--- employees/server/main.js
+++ employees/server/main.js
@@ -1,25 +1,8 @@
-
-import _ from 'lodash';
+// Only executed on the server
 import { Meteor } from 'meteor/meteor';
-import { Employees } from '..imports/collections/employees';
-import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
-  // code to run on server at startup
   // Great place to generate data
 
-  // Chech to see if data exists in the collection
-   // See if the collection has any records
-  const numberRecords = Employees.find({}).cont();
-  if (!numberRecords) {
-    // Generate some daa...
-    _.times(5000, () => {
-      const { name, email, phone } = helpers.createCard();
-
-      Employees.insert({
-        name, email, phone,
-        avatar: image.avatar()
-      });
-    });
-  };
 });
+
