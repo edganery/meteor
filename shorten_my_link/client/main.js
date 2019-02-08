5ac0f01d8f4877e0049954b4a5a21e8f1c34e771
Edgard <edganery@hotmail.com>
2019-02-08 00:00:32 -0200

    setup S4 shortenmylink npm and rm .json

--- shorten_my_link/client/main.js
+++ shorten_my_link/client/main.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from './components/header';
-import LinkCreate from './components/link_create';
 const App = () => {
-  return (
-     <div>
-       <Header />
-       <LinkCreate/>
-     </div>
-  );
+    return (
+      <h1>heeeeey all</h1>
+    );
 };
 
 Meteor.startup(() => {
-  ReactDOM.render (<App />, document.querySelector('.render-target'));
+    ReactDOM.render(<App />, document.querySelector('.render-target'));
 });
