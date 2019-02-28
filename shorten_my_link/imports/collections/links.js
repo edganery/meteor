7f9c099195c95d2cc91e85736b6e774128e624b9
Edgard <edganery@hotmail.com>
2019-02-28 07:13:03 -0300

    section 4 learn 64: create a method in collections links

--- shorten_my_link/imports/collections/links.js
+++ shorten_my_link/imports/collections/links.js
@@ -1,5 +1,5 @@
 import { Mongo } from 'meteor/mongo';
-
+// first find the imports and create a meteor method for ur link
 Meteor.methods({
   'links.insert': function (url) {
     console.log('attempting to save', url);
